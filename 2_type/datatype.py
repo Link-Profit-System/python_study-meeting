a71ca38305e705bd1af51e3daa190ed4cdf10d36
ryo_ara <ryouhei123123@gmail.com>
2021-10-13 16:58:59 +0900

    一旦データ型の部分作成

---
+++ 2_type/datatype.py
@@ -0,0 +1,28 @@
+
+
+# 整数型
+i = 0
+print(type(i))
+
+# 浮動小数点型
+f = 0.2
+print(type(f))
+
+# 文字列型
+s = "文字列"
+print(type(i))
+
+b = True
+print(type(b))
+
+# タプル型
+tp = (1,"aaaa")
+print(type(tp))
+
+# リスト型
+lst = [1,2,3]
+print(type(lst))
+
+# 辞書型
+dct = {"Dict1":1,"Dict2":2}
+print(type(dct))
